0dc7bd7f38c52b3f3249db29d2768024de7a02a0
maksimilchenko <maxilchenko5777@gmail.com>
2025-07-01 13:25:00 +0200

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hallo von CoinStarrr Bot!")
+# Dein echter Telegram Bot Token
+TOKEN = '7404537703:AAEtSSQBE6Cuf3TpLEa0bJScUtol4TB66o0'
+bot = telebot.TeleBot(TOKEN)
+
+# Begrüßung bei /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Willkommen bei CoinStarrr! ✨")
+
+# Echo: antwortet auf alles
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+# Bot starten
+bot.infinity_polling()
